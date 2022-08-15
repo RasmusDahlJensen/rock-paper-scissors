98139c70dd39fa108b121aba90cc313be2bd4355
Rasmus Dahl Jensen <rasmusdj96@gmail.com>
2022-08-15 18:18:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,45 +24,54 @@ function playRound() {
     if (playerSelection == "rock") {
         switch (computerSelection) {
             case "rock":
-                    console.log("Computer chose rock...");
+                    console.log("You chose rock...");
+                    console.log("The computer also chose rock...");
                     return "draw"
 
                 
             case "paper":
-                    console.log("Computer chose paper...");
+                    console.log("You chose rock...");
+                    console.log("The computer chose paper...");
                     return "lost"
 
 
             case "scissors":
-                    console.log('Computer chose scissors');
+                    console.log("You chose rock...");
+                    console.log('The computer chose scissors');
                     return "win"
         }
     } else if (playerSelection == "paper") {
         switch (computerSelection) {
             case "rock":
-                    console.log("Computer chose rock...");
+                    console.log("You chose paper...");
+                    console.log("The computer chose rock...");
                     return "win"
                 
             case "paper":
-                    console.log("Computer chose paper...");
+                    console.log("You chose paper...");
+                    console.log("The computer also chose paper...");
                     return "draw"
 
             case "scissors":
-                    console.log('Computer chose scissors');
+                    console.log("You chose paper...");
+                    console.log('The computer chose scissors');
                     return "lost"
         }
     } else if (playerSelection == "scissors") {
         switch (computerSelection) {
             case "rock":
-                    console.log("Computer chose rock...");
+                    console.log("You chose scissors...");
+                    console.log("The computer chose rock...");
                     return "lost"
                 
             case "paper":
-                    console.log("Computer chose paper...");
+                    console.log("You chose scissors...");
+                    console.log("The computer chose paper...");
                     return  "win"
 
             case "scissors":
-                    console.log('Computer chose scissors');
+                    console.log("You chose scissors...");
+                    console.log("The computer also chose scissors...");
                     return "draw"
         }
     }
@@ -75,28 +84,48 @@ function playRound() {
         let playerResult = 0
         let computerResult = 0
         let drawResult = 0
-        
 
         for(let i = 0; i < 5; i++){
 
                 switch (playRound()) {
                     case "win":
                         playerResult++
+                        console.log("You win!!!");
                         console.log("Player win count: " + playerResult);
                         break;
 
                     case "lost":
                         computerResult++
+                        console.log("You lost...");
                         console.log("Computer win count: " + computerResult);
                         break;
 
                     case "draw":
                         drawResult++
+                        console.log("It's a draw.");
                         console.log("Draw count: " + drawResult);
                         break;
             
                 }
+                if (i == 4) {
+                    console.clear()
+                    console.log("You won " + playerResult + "times");
+                    console.log("The computer won " + computerResult + "times");
+                    console.log("You drew "+ drawResult + "times");
+    
+                    if (playerResult>computerResult) {
+                        console.log("Congratulations you win the game!!");
+                    } else if (computerResult>playerResult) {
+                        console.log("You fucking lost m8");
+                    } else{
+                        console.log("It's a draw.");
+                    }
+                }
+
+
         }
+
+
 
     }
     game()
